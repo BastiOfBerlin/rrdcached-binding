4b86064506596487c30bef6849bc0e0dea65b7fb
Sebastian List <basti@mlist-online.de>
2015-12-14 20:31:33 +0100

    initial commit

---
+++ rrdcached.js
@@ -0,0 +1,90 @@
+'use strict';
+var net = require('net');
+
+if(typeof String.prototype.startsWith === 'undefined'){
+	String.prototype.startsWith = function(prefix) {
+		return this.indexOf(prefix) === 0;
+	}
+};
+
+var RRDCache = function(){
+
+	var client = null;
+	var buffer = '';
+	var lastReply = null;
+
+	this.connect = function(address, callback){
+		var self = this;
+		if(typeof address === 'undefined'){
+			address = 'unix:/tmp/rrdcached.sock';
+		} else if (address === ''){
+			address = ':42217';
+		}
+		var options = (address.startsWith('unix:/') ? {path: address.substring(5)} : {host: address.substring(0,address.indexOf(':')), port: address.substring(address.indexOf(':') + 1)});
+		console.log(options);
+		self.client = net.createConnection(options);
+		self.client.on('connect', function() {
+			this.setEncoding('ascii');
+			console.log('on connect');
+			callback();
+		});
+	};
+
+	this.write = function(command, callback){
+		var self = this;
+		var status = null;
+		if(self.client != null && command != null){
+			console.log('writing data');
+			// append newline to terminate command
+			if(command.substring(command.length - 1) != "\n"){
+				command += "\n";
+			}
+			if(command === 'QUIT\n'){
+				self.client.end(command, 'ascii');
+			} else {
+				self.client.write(command, 'ascii');
+			}
+			self.client.on('data', function(data){
+				console.log("received: '"+data+"'");
+				self.buffer += data;
+				if(status != null){
+					status = self.buffer.substring(0, self.buffer.indexOf(' '));
+				}
+				var receivedAll = status < 0 || ((self.buffer.match(/\n/g) || []).length == status + 1);
+				
+				if(receivedAll){
+					self.buffer = '';
+					status = null;
+					callback(null, self.processData(data));
+				}
+			});
+		} else {
+			callback(new Error('Not Connected!'));
+		}
+	};
+	
+	this.processData = function(data){
+		var lines = data.split('\n');
+		var statusIdx = lines[0].indexOf(' ');
+		var status = parseInt(lines[0].substring(0, statusIdx));
+		var error = status < 0 ? true : false;
+		var info = Array();
+		if(error != false){
+			for(i = 1; i < status; i++){
+				info.push(lines[i]);
+			}
+		}
+		this.lastReply = {
+			statuscode: status,
+			status: lines[0].substring(statusIdx + 1),
+			error: error,
+			info: info
+		};
+		return this.lastReply;
+	};
+	
+	this.getLastReply = function(){
+		return this.lastReply;
+	};
+};
+module.exports = RRDCache;
